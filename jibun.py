d88ff8b5e03a567b137d864299db7c3ef02c2c62
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-01-05 13:30:13 +0100

    Add adding a log behaviour

--- jibun.py
+++ jibun.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import date
+import calendar
 
 # Creates journey starting point.
 base_path = Path(".")
@@ -25,3 +26,18 @@ while user_input != "-1" and user_input != "2":
           2 - Exit
           """)
     user_input = input("I want to... >> ")
+    match user_input:
+        case "1":
+            # Get the current month and search for file, if not exists then create it.
+            current_month_name = calendar.month_name[date.today().month]
+
+            month_path = Path.joinpath(journey_path, str(date.today().year), current_month_name + '.txt')
+            if not (month_path.exists()):
+                print(month_path, "does not exist. Creating the path.")
+                Path.touch(month_path)
+            # Add the log by comparing dates. If the log exists, update the log
+            with month_path.open("w+") as f:
+                f.writelines(["Test\n", "Test2\n"])
+                print(f.readlines())
+            # Log format: Line -> Current Date, Kcal and Protein Limits, Kcal and Protein
+            # Last Line -> Totals
