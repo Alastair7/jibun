6dcc293cba54cd60bd1c49e76664a8e81566b324
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-01-07 20:29:14 +0100

    refactor jibun.py and create a helper file

--- jibun.py
+++ jibun.py
@@ -1,80 +1,34 @@
 from pathlib import Path
 from datetime import date
 import calendar
+import journal_helper
 
-# Creates journey starting point.
+current_month = date.today().month
+month_name = calendar.month_name[current_month]
+
 base_path = Path(".")
 journey_path = Path.joinpath(base_path, Path("journey"))
+month_file = Path.joinpath(journey_path, str(date.today().year), month_name + ".txt")
+
 menu_options = """
           1 - Add my daily log
           2 - Set my energy limit
           3 - Exit
           """
+
+journal_helper.init_journey()
+
 energy_limit = ""
-current_month_name = calendar.month_name[date.today().month]
+adventurer_input = ""
 
-def set_energy_limit():
-    kcal_limit = input("Kcal limit >> ")
-    protein_limit = input("Protein limit >> ")
-
-    energy_limit = ",".join([kcal_limit, protein_limit])
-
-    return energy_limit
-
-
-def add_log(calories, protein):
-    month_path = Path.joinpath(journey_path, str(date.today().year), current_month_name + '.txt')
-    if not month_path.exists():
-        print(month_path, "does not exist. Creating the path.")
-        Path.touch(month_path)
-
-    if len(energy_limit) == 0:
-        print("Seems that you don't have any energy limit set, embrace your limits.")
-        energy_limit = set_energy_limit()
-
-    log_data = ','.join([calories, protein])
-    cur_date = date.today().isoformat()
-
-    base_log = cur_date + " " +  energy_limit + " " + log_data
-
-    with open(month_path) as f:
-        file_lines = f.readlines()
-
-    for line in file_lines:
-        if cur_date in line:
-            line_index = file_lines.index(line)
-            file_lines[line_index] = base_log
-            break
-        else:
-            file_lines.append(base_log + "\n")
-            break
-    else:
-        file_lines.append(base_log + "\n")
-
-    with open(month_path, "w+") as f:
-        f.writelines(file_lines)
-if not Path.exists(journey_path):
-    print("Journey not found.")
-    print("Starting a new journey in path:", journey_path.absolute())
-    Path.mkdir(journey_path)
-    
-    for item in journey_path.iterdir():
-        item.exists() and item.is_dir()
-        
-    print('Creating your starting year directory as well:', date.today().year)
-    starting_year_path = Path.joinpath(journey_path, str(date.today().year))
-    Path.mkdir(starting_year_path)
-
-user_input = ""
-
-while user_input != "-1" and user_input != "3":
+while adventurer_input != "-1" and adventurer_input != "3":
     print("Hey Adventurer! What would you like to do?")
     print(menu_options)
-    user_input = input("I want to: ")
-    match user_input:
+    adventurer_input = input("I want to: ")
+    match adventurer_input:
         case "1":
             calories = input('Kcal >> ')
-            protein_input = input('Protein >> ')
-            add_log(calories, protein_input)
+            protein = input('Protein >> ')
+            journal_helper.add_daily_log(month_file, calories, protein, energy_limit)
         case "2":
-            energy_limit = set_energy_limit()
+            energy_limit = journal_helper.set_adventurer_limits()
---
+++ journal_helper.py
@@ -0,0 +1,52 @@
+import calendar
+from datetime import date
+from pathlib import Path
+
+def init_journey():
+    journey_path = Path("./journey")
+    if not Path.exists(journey_path):
+        print("Your journey has not started yet...")
+        print("Starting a new journey in: ", journey_path.absolute())
+        Path.mkdir(journey_path)
+
+        current_year = str(date.today().year)
+        year_path = Path.joinpath(journey_path, current_year)
+        Path.mkdir(year_path, parents=True, exist_ok=True)
+
+        current_month = date.today().month
+        month_name = calendar.month_name[current_month]
+        month_file = Path.joinpath(year_path, month_name)
+        Path.touch(month_file + ".txt", parents=True, exist_ok=True)
+
+def set_adventurer_limits():
+    kcal = input('Kcal Limit: ')
+    protein = input('Protein Limit: ')
+
+    return ",".join([kcal, protein]) 
+
+def add_daily_log(month_file, kcalories, protein, energy_limit):
+    if len(energy_limit) == 0:
+        print("Seems that you don't have any energy limit set, embrace your limits.")
+        energy_limit = set_adventurer_limits()
+
+    log_data = ",".join([kcalories, protein])
+    cur_date = date.today().isoformat()
+
+    base_log = cur_date + " " + energy_limit + " " + log_data
+
+    with open(month_file) as file:
+        file_lines = file.readlines()
+    
+    for line in file_lines:
+        if cur_date in line:
+            line_index = file_lines.index(line)
+            file_lines[line_index] = base_log
+            break
+        else:
+            file_lines.append(base_log + "\n")
+            break
+    else:
+        file_lines.append(base_log + "\n")
+
+    with open(month_file, "w+") as f:
+        f.writelines(file_lines)
